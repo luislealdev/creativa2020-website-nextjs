887a74a338f82c29f46ad4f5707495889080d75c
Luis Leal <luisrrleal@gmail.com>
2023-01-23 17:54:53 -0600

    Add Rods article to recent articles

--- data/recentPosts/recentArticles.ts
+++ data/recentPosts/recentArticles.ts
@@ -22,11 +22,26 @@ export const recentArticles: article[] = [
       metaTitle: "Como vender lentes online",
       metaDesc: "Como vender lentes online",
       socialImage: "https://img.freepik.com/foto-gratis/negocio-compras-linea-concepto-venta_1421-238.jpg?w=1480&t=st=1673561891~exp=1673562491~hmac=92487973a0568c4576f5a8469f9279c2b1823a03eeb586953a48965df9b56393",
-      date: "12 enero, 2022",
+      date: "12 enero, 2023",
       author: "Luis Leal",
       authorImg: "img/team/Luis.png",
       category: "Ventas online",
       tags: ['ventas, incrementa ventas, dinero, lentes, vender lentes online, comoo vender lentes online, opticas, marketing']
+
+    }
+  },
+  {
+    title: "la-importancia-de-la-capacitacion-en-el-sector-optico",
+    content: {
+      title: "La importancia de la capacitación en el sector óptico",
+      metaTitle: "La importancia de la capacitación en el sector óptico",
+      metaDesc: "La importancia de la capacitación en el sector óptico",
+      socialImage: "https://img.freepik.com/foto-gratis/orador-musulman-femenino-dando-presentacion-salon-taller-audiencia-o-sala-conferencias-cierrese-arriba-mano-que-senala-marcador-evento-conferencia-formacion-educacion-diversidad-concepto-inclusivo_155003-39222.jpg?w=1380&t=st=1674517310~exp=1674517910~hmac=2f4a37b72ee201c83955e56b784eef978aeb122cc8e4b90a71ea09b826687eab",
+      date: "23 enero, 2023",
+      author: "Rod Castañeda",
+      authorImg: "https://cursosdeoptometria.com.mx/img/rod11.jpg",
+      category: "Consejos",
+      tags: ['ventas','incrementa ventas','dinero','capacitacion','planeacion','opticas','habilidades','herramientas','empleados']
 
     }
   }
