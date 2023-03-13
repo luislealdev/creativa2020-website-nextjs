5b2e244545672e1ddac71bb73b07110ac98d99f1
Luis Leal <luisrrleal@gmail.com>
2023-03-13 16:35:07 -0600

    Add keywords

--- data/keywords/index.ts
+++ data/keywords/index.ts
@@ -1,5 +1,8 @@
 export const keywords = [
     "Agencia de marketing digital",
     "Agencia de marketing y agencia de publicidad",
-    "Agencia digital"
+    "Agencia digital",
+    "Agencia de publicidad",
+    "Marketing digital",
+    ""
 ]
